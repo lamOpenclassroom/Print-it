f0958bc4ae23186d884c5bdf1e51e790e8524a57
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-02-24 21:22:07 +0100

    Correction image + dots

--- assets/script.js
+++ assets/script.js
@@ -1,117 +1,106 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
-		"point":"<div class='dot'></div>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>",
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-		"point":"<div class='dot'></div>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
-		"point":"<div class='dot'></div>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
-		"point":"<div class='dot'></div>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>",
 	}
 ]
-//console.log(slides[0].image);
-//console.log(slides[0].tagLine);
-let arrowLeft = document.querySelector(".arrow_left");
-let arrowRight = document.querySelector(".arrow_right");
-let image = document.querySelector(".banner-img");
-let paragraph = document.querySelector("p");
-let dots = document.querySelector(".dots");
-let tabSlide = slides.length;
-let banner = document.getElementById("banner");
+
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+const paragraph = document.querySelector("p");
+const dotsElement = document.querySelector(".dots");
+
+const numberOfSlide = slides.length;
+const banner = document.getElementById("banner");
 let count = 0;
-//console.log(paragraph);
-//console.log(paragraph.innerHTML);
 
+banner.insertAdjacentHTML("beforeend", `<img class="banner-img" src="./assets/images/slideshow/${slides[0].image}"  alt="Image de Print-it">`);
+let image = document.querySelector(".banner-img");
+console.log(image)
 
-//utilise les for pour afficher les dots, faire une boucle sur le nombre d'entrée qui feront apparaitre les dots
-
-//dots Inclure les Div dot dans la div Dots
-for (let i = 0; i < tabSlide;i++){
-	//console.log(slides[i].point);
-	dots.insertAdjacentHTML("beforeend",`${slides[i].point}`); 
+function dotElement() {
+	for (let i = 0; i < numberOfSlide; i++) {
+		dotsElement.insertAdjacentHTML("beforeend", `<div class="dot"></div>`);
+	}
 }
-let divDot = document.querySelectorAll('.dot');
-let lastDot = divDot[3];
-let firstDot = divDot[0];
-console.log(lastDot)
-console.log(firstDot)
-console.log(tabSlide)
-//clic droit
-arrowRight.addEventListener("click", (e)=>{
-	console.log(index)
-	console.log(numIndex)
+dotElement();
+const dot = document.querySelector(".dot");
+console.log(dot)
+
+
+
+function changePhoto(image) { //j'ai retiré count du paramètre de la fonction du return aussi
+	if (count == 0) {
+		image = image.setAttribute("src", "./assets/images/slideshow/slide1.jpg");
+	}
+	else if (count == 1) {
+		image = image.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
+		//console.log("numIndex = 1 donc image =");
+	} else if (count == 2) {
+		image = image.setAttribute("src", "./assets/images/slideshow/slide3.jpg");
+		//console.log("numIndex = 2 donc image =");
+	} else if (count == 3) {
+		image = image.setAttribute("src", "./assets/images/slideshow/slide4.png");
+		//console.log("numIndex = 3 donc image =");	
+	}
+	return image;
+}
+
+//dot_selected
+function dotSelect(){
 	numIndex++;
-	index++;
-	displaySlider(numIndex);
-	deletDotR();
-	lastClick();
-	return numIndex,index;
-});
-
-//clic gauche
-arrowLeft.addEventListener("click", (e)=>{
-	numIndex--;	
-	index--;
-	displaySlider(numIndex);
-	deletDotL();
-	firstClick();
-	//console.log(count);
-});
-
-const displaySlider =(numIndex)=>{ 
-	//console.log(numIndex);
-	banner.insertAdjacentHTML("beforeend",`<img class="banner-img" src="./assets/images/slideshow/${slides[numIndex].image}" alt="${slides[numIndex].image}">`);
-	paragraph.innerHTML = (slides[numIndex].tagLine);
-	divDot[numIndex].classList.add("dot_selected");
-	return numIndex; 
-}
-let numIndex = displaySlider(0);
-
-function deletDotL(){
-	for(slides[numIndex].point of slides){
-		divDot[numIndex+1].classList.remove("dot_selected");
+	count++;
+	if (numIndex === count) {
+		dot.classList.add("dot_selected")
+	}else{
+		dot.classList.remove("dot_selected")
 	}
 }
 
-function deletDotR(){
-	for(slides[numIndex].point of slides){
-		divDot[index].classList.remove("dot_selected");
-	}return index;
-}
-let index = -1;
 
-function lastClick(){
-	if (numIndex >= tabSlide -1){
+
+//clic droit
+arrowRight.addEventListener("click", (e) => {  //faire la boucle
+	numIndex++;
+	count++;
+	displaySlider(numIndex);
+	changePhoto(image);
+	dotSelect();
+	if (numIndex >= numberOfSlide - 1) {
 		numIndex = -1;
-	}return numIndex;	
-}
-
-function firstClick(){
-	if (numIndex <= 0){
-		numIndex = +1;
+		count = -1;
 	}
+	console.log(numIndex, count)
+});
+//clic gauche
+arrowLeft.addEventListener("click", (e) => {  //faire la boucle
+	numIndex--;
+	count--;
+	displaySlider(numIndex);
+	changePhoto(image);
+	/*if (numIndex <= 0){
+		numIndex = -1;
+		count = -1;
+	}*/
+});
+
+const displaySlider = (numIndex = 0) => {
+	paragraph.innerHTML = (slides[numIndex].tagLine);
+	return numIndex;
 }
+let numIndex = displaySlider();
 
 
-
-
-
-	
-
-
-
-
-
-
-
+//dot[numIndex].classList.add("dot_selected");
