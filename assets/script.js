d54fc73130d965aaa3fc0b85716b11e3c28fe522
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-02-26 16:02:37 +0100

    correction du slide des dots vers la gauche

--- assets/script.js
+++ assets/script.js
@@ -25,9 +25,9 @@ const numberOfSlide = slides.length;
 const banner = document.getElementById("banner");
 let count = 0;
 
+//initialise les images à l'index 0
 banner.insertAdjacentHTML("beforeend", `<img class="banner-img" src="./assets/images/slideshow/${slides[0].image}"  alt="Image de Print-it">`);
 let image = document.querySelector(".banner-img");
-console.log(image)
 
 function dotElement() {
 	for (let i = 0; i < numberOfSlide; i++) {
@@ -35,73 +35,84 @@ function dotElement() {
 	}
 }
 dotElement();
+
+//constante qui récupère un tableau de dot.
 const dot = document.querySelectorAll(".dot");
 console.log(dot)
+
+//initialise l'index 0 à dot_selected
 dot[0].classList.add("dot_selected");
 
-
-function changePhoto(image) { 
+//Slide des images
+function displayImage(image) { 
 	if (count == 0) {
-		image = image.setAttribute("src", "./assets/images/slideshow/slide1.jpg");
+		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
 	}
 	else if (count == 1) {
-		image = image.setAttribute("src", "./assets/images/slideshow/slide2.jpg");
-		//console.log("numIndex = 1 donc image =");
+		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
+		//console.log("count = 1 donc image =");
 	} else if (count == 2) {
-		image = image.setAttribute("src", "./assets/images/slideshow/slide3.jpg");
-		//console.log("numIndex = 2 donc image =");
+		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
+		//console.log("count = 2 donc image =");
 	} else if (count == 3) {
-		image = image.setAttribute("src", "./assets/images/slideshow/slide4.png");
-		//console.log("numIndex = 3 donc image =");	
+		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
+		//console.log("count = 3 donc image =");	
 	}
-	return image;
 }
 
-//dot_selected
-function dotSelect(item) {
-	console.log(item);
-	item.classList.remove("dot_selected")
-	dot[numIndex].classList.add("dot_selected");
-	//item = item.classList.add("dot_selected");
-    /*if (item.classList.contains("dot_selected")) {
-        item.classList.remove("dot_selected");
-    }
-    else {
-        item.classList.add("dot_selected");
-    }*/
-}
-
-
-//clic droit
-arrowRight.addEventListener("click", (e) => {  //faire la boucle
-	numIndex++;
-	count++;
-	displaySlider(numIndex);
-	changePhoto(image);
-	dot.forEach(dotSelect);
-	if (numIndex >= numberOfSlide - 1) {
-		numIndex = -1;
-		count = -1;
-	}
-	console.log(numIndex, count)
-});
-//clic gauche
-arrowLeft.addEventListener("click", (e) => {  //faire la boucle
-	numIndex--;
-	count--;
-	displaySlider(numIndex);
-	changePhoto(image);
-	if (numIndex <= 0){
-		numIndex = -2;
-		count = -2;
-	}
-});
-
+//Slide du Texte
 const displaySlider = (numIndex = 0) => {
 	paragraph.innerHTML = (slides[numIndex].tagLine);
 	return numIndex;
 }
 let numIndex = displaySlider();
 
+//dot_selected
+function dotSelect(item) {
+	item.classList.remove("dot_selected")
+	dot[numIndex].classList.add("dot_selected");
+}
 
-//dot[numIndex].classList.add("dot_selected");
+//Boucle infini vers la droite
+function returnFirst(){
+	if (numIndex >= numberOfSlide -1) {
+		console.log("vous allez revenir à l'index 0");
+		numIndex = -1;
+		count = -1;
+	}
+}
+arrowRight.addEventListener("click", returnFirst);
+
+//Boucle infini vers la gauche
+function returnLast(){
+	if (numIndex <= 0) {
+		console.log("vous allez revenir à l'index 3");
+		numIndex = numberOfSlide;
+		count = numberOfSlide;
+	}
+}
+arrowLeft.addEventListener("click", returnLast); //Boucle si je clic à gauche et mon index et < ou = 0 => l'index renvoit 3
+
+//clic droit
+arrowRight.addEventListener("click", (e) => {  
+	numIndex++;
+	count++;
+	displaySlider(numIndex);
+	displayImage(image);
+	dot.forEach(dotSelect); //Pour chaque item de mon tableau dot renvoyer la fonction dotSelect
+	console.log(numIndex, count)
+});
+
+//clic gauche
+arrowLeft.addEventListener("click", (e) => {  
+	numIndex--;
+	count--;
+	displaySlider(numIndex);
+	displayImage(image);
+	dot.forEach(dotSelect);
+	//returnLast() Erreur à ne plus refaire
+	console.log(numIndex, count)
+});
+
+
+
