011b0b6938e34c2d0f992266c094ac0c8df6cf03
lamtaoui mohamed <el-hafid-@hotmail.fr>
2023-02-28 16:20:41 +0100

    code js affiné et rectif erreur ajout ou suppr img

--- assets/script.js
+++ assets/script.js
@@ -23,12 +23,13 @@ const paragraph = document.querySelector("p");
 const dotsElement = document.querySelector(".dots");
 const numberOfSlide = slides.length;
 const banner = document.getElementById("banner");
-let count = 0;
 
-//initialise les images à l'index 0
+//inclure la balise img dans mon HTML et initialiser les images à l'index 0.
 banner.insertAdjacentHTML("beforeend", `<img class="banner-img" src="./assets/images/slideshow/${slides[0].image}"  alt="Image de Print-it">`);
-let image = document.querySelector(".banner-img");
+//récupèrer la balise img.
+const image = document.querySelector(".banner-img");
 
+//Parcourir l'array pour afficher les dots proportionellement au nombre d'images.
 function dotElement() {
 	for (let i = 0; i < numberOfSlide; i++) {
 		dotsElement.insertAdjacentHTML("beforeend", `<div class="dot"></div>`);
@@ -38,80 +39,57 @@ dotElement();
 
 //constante qui récupère un tableau de dot.
 const dot = document.querySelectorAll(".dot");
-console.log(dot)
+console.log(dot);
 
-//initialise l'index 0 à dot_selected
+//initialiser le format dot_selected à l'index 0. 
 dot[0].classList.add("dot_selected");
 
-//Slide des images
-function displayImage(image) { 
-	if (count == 0) {
-		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
-	}
-	else if (count == 1) {
-		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
-		//console.log("count = 1 donc image =");
-	} else if (count == 2) {
-		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
-		//console.log("count = 2 donc image =");
-	} else if (count == 3) {
-		image = image.setAttribute("src", "./assets/images/slideshow/"+slides[count].image);
-		//console.log("count = 3 donc image =");	
-	}
-}
-
-//Slide du Texte
+//Fonction Slide du Texte et image.
 const displaySlider = (numIndex = 0) => {
 	paragraph.innerHTML = (slides[numIndex].tagLine);
+	image.setAttribute("src", "./assets/images/slideshow/"+slides[numIndex].image);
 	return numIndex;
 }
 let numIndex = displaySlider();
 
-//dot_selected
+//Fonction dotSelected à assigner à la méthode forEach.
 function dotSelect(item) {
 	item.classList.remove("dot_selected")
 	dot[numIndex].classList.add("dot_selected");
 }
 
-//Boucle infini vers la droite
+//Fonction Boucle infini vers la droite assignée à la fonction (anonyme) click.
 function returnFirst(){
 	if (numIndex >= numberOfSlide -1) {
 		console.log("vous allez revenir à l'index 0");
 		numIndex = -1;
-		count = -1;
 	}
 }
 arrowRight.addEventListener("click", returnFirst);
 
-//Boucle infini vers la gauche
+//Fonction Boucle infini vers la gauche assignée à la fonction (anonyme) click.
 function returnLast(){
 	if (numIndex <= 0) {
-		console.log("vous allez revenir à l'index 3");
+		console.log("vous allez revenir au dernier index");
 		numIndex = numberOfSlide;
-		count = numberOfSlide;
 	}
 }
-arrowLeft.addEventListener("click", returnLast); //Boucle si je clic à gauche et mon index et < ou = 0 => l'index renvoit 3
+arrowLeft.addEventListener("click", returnLast); 
 
 //clic droit
 arrowRight.addEventListener("click", (e) => {  
 	numIndex++;
-	count++;
 	displaySlider(numIndex);
-	displayImage(image);
 	dot.forEach(dotSelect); //Pour chaque item de mon tableau dot renvoyer la fonction dotSelect
-	console.log(numIndex, count)
+	console.log(numIndex);
 });
 
 //clic gauche
 arrowLeft.addEventListener("click", (e) => {  
 	numIndex--;
-	count--;
 	displaySlider(numIndex);
-	displayImage(image);
 	dot.forEach(dotSelect);
-	//returnLast() Erreur à ne plus refaire
-	console.log(numIndex, count)
+	console.log(numIndex);
 });
 
 
